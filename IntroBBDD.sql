e855fbd800472f713f98e8b600ca72734cfec5ee
Alex <alexandre.p.a.94@gmail.com>
2026-09-07 20:44:03 +0200

    ejercicio 2

---
+++ IntroBBDD.sql
@@ -0,0 +1,10 @@
+-- Ejercicio 1: Vuelos con estado On Time
+SELECT flight_id, route_no, status 
+FROM flights 
+WHERE status = 'On Time';
+
+-- Ejercicio 2: Reservas con un importe total superior a 1.000.000
+SELECT * 
+FROM bookings 
+WHERE total_amount >= 1000000;
+
